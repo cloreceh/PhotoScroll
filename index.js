988fbb9f744b90d293030e5eceffdd4e61332525
cloreceh <hammitt1@msu.edu>
2025-02-24 00:06:53 +0000

    scrollreveal updates

---
+++ index.js
@@ -0,0 +1,129 @@
+ScrollReveal().reveal('.stonehaven', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.stirling', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.stirlingView', { 
+    duration: 1000, 
+    easing: 'ease-in-out',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.cannon', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.5,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.wallaceMonument', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.curry', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.Urquhart', { 
+    duration: 1000, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.castleGrounds', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+    origin: 'left',
+  });
+  ScrollReveal().reveal('.highlands', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.dot', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+    distance: '50px',
+  });
+  ScrollReveal().reveal('.stAndrews', { 
+    duration: 700, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+  });
+  ScrollReveal().reveal('.ceremonyFar', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.5,
+    distance: '50px',
+  });
+  ScrollReveal().reveal('.ceremony', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.5,
+    scale: 1.25,
+  });
+  ScrollReveal().reveal('.UnderTree', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.5,
+    scale: 1.25,
+  });
+  ScrollReveal().reveal('.confetti', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.5,
+    scale: 1.25,
+  });
+  ScrollReveal().reveal('.bouquet', { 
+    duration: 500, 
+    easing: 'ease-in',
+    viewFactor: 0.9,
+    opacity: 0.5,
+    distance: '50px',
+    reset: true,
+  });
+  ScrollReveal().reveal('.glasgow', { 
+    duration: 500, 
+    easing: 'ease-in',
+    reset: true,
+    viewFactor: 0.9,
+    opacity: 0.25,
+    distance: '50px',
+  });
+
+  ScrollReveal().sync();
